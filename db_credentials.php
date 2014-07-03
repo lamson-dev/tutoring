76f859504ea9ccb4031aa387cd53b06e5304770c
Son Nguyen <son@lamson.me>
2014-07-03 14:24:21 +0200

    added our database credential

--- db_credentials.php
+++ db_credentials.php
@@ -1,6 +1,6 @@
 <?php
-$db_host = "db.cip.gatech.edu"; //"dbadmin.cip.gatech.edu"
-$db_database = "CONTRIB_dataviz";
-$db_username = "contrib_dataviz";
-$db_password = "9Yw96VRSnaDXV3Ay";
+$db_host = "academic-mysql.cc.gatech.edu"; //"https://academic-mysql.cc.gatech.edu/phpmyadmin/"
+$db_database = "cs4400_Group_53";
+$db_username = "cs4400_Group_53";
+$db_password = "oyyITEE4";
 ?>
